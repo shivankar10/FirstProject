a3999b15f1b5059d331de7051831808b1ddcecc5
Shivankar Srivastava <shivankar.10@outlook.com>
2021-04-15 15:14:41 +0530

    first commit

---
+++ src/main/java/com/example/demo/Alien.java
@@ -0,0 +1,52 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope(value="prototype")
+public class Alien {
+	private int aid;
+	private String aname;
+	private String tech;
+	@Autowired
+	@Qualifier("lap2")
+	private Laptop laptop;
+	
+	
+	public Alien() {
+		super();
+		System.out.println("Object Created!");
+	}
+	public Laptop getLaptop() {
+		return laptop;
+	}
+	public void setLaptop(Laptop laptop) {
+		this.laptop = laptop;
+	}
+	public int getAid() {
+		return aid;
+	}
+	public void setAid(int aid) {
+		this.aid = aid;
+	}
+	public String getAname() {
+		return aname;
+	}
+	public void setAname(String aname) {
+		this.aname = aname;
+	}
+	public String getTech() {
+		return tech;
+	}
+	public void setTech(String tech) {
+		this.tech = tech;
+	}
+	
+	public void show() {
+		System.out.println("Inside Show function!");
+		laptop.compile();
+	}
+}
